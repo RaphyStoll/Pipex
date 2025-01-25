e9511c524d85bde3cad6fc1b4b48cd540bc4d48c
RaphyStoll <raphael.ferreira@ik.me>
2025-01-25 20:49:12 +0100

    add/struct.h

---
+++ include/main/struct.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 14:43:30 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/01/25 19:15:33 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+typedef struct s_data
+{
+	int     fd_input;
+	int     fd_output;
+	int     fd_pipe[2];
+	char    **cmd1;
+	char    **cmd2;
+}   t_data;
+
+typedef struct s_split
+{
+   char	**words;
+   int		word_count;
+   int		start;
+   int		len;
+}	t_split;
+
+#endif
