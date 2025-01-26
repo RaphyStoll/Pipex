dbe7b737fec8b326aecb0903e70c25825273e90a
RaphyStoll <raphael.ferreira@ik.me>
2025-01-26 02:10:21 +0100

    add/ init et process

--- source/main/main.c
+++ source/main/main.c
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 21:40:16 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/01/26 00:04:13 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/main/fonction.h"
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
@@ -6,5 +19,7 @@ int main(int argc, char **argv)
 
 	data = NULL;
 	arg_verif(argc, argv);
-	init_data(data, argv);
+	data = init_data(data, argv);
+	proces(data);
+	//printf_struct(data);
 }
