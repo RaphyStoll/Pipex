b71e754b3f031d42199eda7f9cd463192662fb93
RaphyStoll <raphael.ferreira@ik.me>
2025-01-26 16:05:48 +0100

    /fix/arg verif

--- source/main/main.c
+++ source/main/main.c
@@ -6,14 +6,13 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:40:16 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/01/26 02:16:27 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/01/26 16:02:07 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main/fonction.h"
 #include <stdio.h>
 
-//$TODO: voir pour mettre les arguments dans la structure et mettre find_path dans le parsing(init)
 //$ coriger printf il semble bugger a plusieurs moment surtout sur %s
 
 int main(int argc, char **argv)
@@ -21,7 +20,9 @@ int main(int argc, char **argv)
 	t_data *data;
 
 	data = NULL;
-	arg_verif(argc, argv);
+	data->argc = argc;
+	data->argv = argv;
+	arg_verif(data);
 	data = init_data(data, argv);
 	proces(data);
 	//printf_struct(data);
