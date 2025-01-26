8324ba1a36ba8cd362dd051c555ad6befd57d2f2
RaphyStoll <raphael.ferreira@ik.me>
2025-01-26 02:08:56 +0100

    fix/gestion d'erreur et ordre de process

--- source/main/init.c
+++ source/main/init.c
@@ -6,30 +6,35 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 15:49:56 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/01/25 19:39:11 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/01/26 01:41:20 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main/fonction.h"
+#include <stdio.h>
 
-void	init_data(t_data *data, char **argv)
+t_data	*init_data(t_data *data, char **argv)
 {
 	data = malloc(sizeof(t_data));
 	if (!data)
 		ft_exit_error("Malloc failed");
-	if (access(argv[1], R_OK))
+	if (access(argv[1], R_OK) == -1)
 		ft_exit_error("Input file not accessible");
 	data->fd_input = open(argv[1], O_RDONLY);
 	if (data->fd_input == -1)
-		ft_exit_error("Open failed");
-	if (access(argv[4], W_OK))
-		ft_exit_error("Output file not accessible");
-	data->fd_output = open("argv[4]", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		ft_exit_error("Open input failed");
+	data->fd_output = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (data->fd_output == -1)
-		ft_exit_error("Open failed");
+		ft_exit_error("Open output failed");
+	if (access(argv[4], W_OK) == -1)
+		ft_exit_error("Output file not accessible");
 	if (pipe(data->fd_pipe) == -1)
 		ft_exit_error("Pipe failed");
+	data->cmd1 = NULL;
+	data->cmd2 = NULL;
+	print_struct(data);
 	parse_cmd(data, argv[2], argv[3]);
+	return (data);
 }
 
 void	parse_cmd(t_data *data, char *cmd1, char *cmd2)
@@ -43,17 +48,32 @@ void	parse_cmd(t_data *data, char *cmd1, char *cmd2)
 		free_array(data->cmd1);
 		ft_exit_error("Malloc cm2 failed");
 	}
+	data->paths =
+		"/bin/:/usr/bin/:/usr/local/bin/:/sbin/:/usr/sbin/:/usr/local/sbin/";
+	print_struct(data);
 }
 
-void	free_array(char **array)
+void print_struct(t_data *data)
 {
-	int	i;
+	int i;
 
+	ft_printf(CYAN"fd_input: %d\n", data->fd_input);
+	ft_printf(CYAN"fd_output: %d\n", data->fd_output);
+	ft_printf(CYAN"fd_pipe[0]: %d\n", data->fd_pipe[0]);
+	ft_printf(CYAN"fd_pipe[1]: %d\n", data->fd_pipe[1]);
 	i = 0;
-	while (array[i])
-	{
-		free(array[i]);
-		i++;
-	}
-	free(array);
+	if (data->cmd1)
+		while (data->cmd1[i]) //*
+		{
+			printf("cmd1[%d]: %s\n", i, data->cmd1[i]);
+			i++;
+		}
+	i = 0;
+	if (data->cmd2)
+		while (data->cmd2[i])
+		{
+			printf("cmd2[%d]: %s\n", i, data->cmd2[i]);
+			i++;
+		}
 }
+
