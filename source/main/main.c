90f8dabbce11f2ee0f608c586ee700ec729935f1
RaphyStoll <raphael.ferreira@ik.me>
2025-01-26 02:16:49 +0100

    add/Todo

--- source/main/main.c
+++ source/main/main.c
@@ -6,12 +6,15 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:40:16 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/01/26 00:04:13 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/01/26 02:16:27 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main/fonction.h"
 #include <stdio.h>
+
+//$TODO: voir pour mettre les arguments dans la structure et mettre find_path dans le parsing(init)
+//$ coriger printf il semble bugger a plusieurs moment surtout sur %s
 
 int main(int argc, char **argv)
 {
