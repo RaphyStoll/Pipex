67c95a423d40740bd9245026c67ab97d85166cc4
RaphyStoll <raphael.ferreira@ik.me>
2025-01-26 02:07:10 +0100

    add/variable char **paths

--- include/main/struct.h
+++ include/main/struct.h
@@ -6,7 +6,7 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 14:43:30 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/01/25 19:15:33 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/01/26 01:39:53 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_data
 	int     fd_pipe[2];
 	char    **cmd1;
 	char    **cmd2;
+	char	*paths;
 }   t_data;
 
 typedef struct s_split
