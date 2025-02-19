8cf4027009db6d5d3a944b65a1386a3046ade01e
RaphyStoll <raphael.ferreira@ik.me>
2025-02-19 15:16:17 +0100

    fix/faute de syntax

--- source/main/utils.c
+++ source/main/utils.c
@@ -6,7 +6,7 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:40:20 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/02/04 17:47:09 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/02/19 14:55:33 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	arg_verif(t_data *data, int argc, char **argv)
 	if (access(argv[1], F_OK) == -1)
 		ft_exit_error(data, "Argv[1] error", ERROR_CMD_NOT_FOUND);
 	fd = open(data->file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd = -1)
+	if (fd == -1)
 		ft_exit_error(data, "Arvg[4] error", ERROR_GENERAL);
 	close(fd);
 }
